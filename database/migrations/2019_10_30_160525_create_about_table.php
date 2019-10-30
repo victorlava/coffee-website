33166c756f2c2b861f0293aae46c22352c0621dd
Victor Lava <hello@victorlava.com>
2019-10-30 18:06:05 +0200

    creating about table migration

---
+++ database/migrations/2019_10_30_160525_create_about_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('about', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('sub_title');
+            $table->string('text');
+            $table->string('media_name');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about');
+    }
+}
