a56d051739351513b387d9997354c676dd8f8963
Victor Lava <hello@victorlava.com>
2019-10-30 18:04:55 +0200

    added home page migration

---
+++ database/migrations/2019_10_30_160130_create_home_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('sub_title');
+            $table->string('text');
+            $table->string('button_text');
+            $table->string('button_link');
+            $table->string('media_name');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home');
+    }
+}
