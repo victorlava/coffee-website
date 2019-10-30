f296355f75b04d50257ef58550b51d9435c4b263
Victor Lava <hello@victorlava.com>
2019-10-30 18:08:53 +0200

    creating product page migration

---
+++ database/migrations/2019_10_30_160630_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('sub_title');
+            $table->string('text');
+            $table->string('media_name');
+            $table->string('meta_title');
+            $table->string('seo_link');
+            $table->string('meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
